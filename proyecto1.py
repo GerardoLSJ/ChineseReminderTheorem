78aca4e1b75f4851436a135e25a72a264b8bdfb6
lsjgerardo <lsjgerardo@gmail.com>
2017-05-14 21:05:27 -0500

    return GCD

--- proyecto1.py
+++ proyecto1.py
@@ -39,10 +39,50 @@ def teoremaChino(elements, modules):
 	return sol
 
 # print(inverso(2, 7))
-print(teoremaChino([2, 3, 2], [3, 5, 7]))
+#print(teoremaChino([2, 3, 2], [3, 5, 7]))
 
 
 #Agregar GCD(x, y)
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+from operator import mul
+import functools
+
+def GCD(a,b):
+	a_primes = prime_factors(a)
+	b_primes = prime_factors(b)
+	max_a = a_primes[len(a_primes)-1]
+	max_b = b_primes[len(b_primes)-1]
+	max_global = max(max_a,max_b)
+	factors_p = []
+	print(a_primes)
+	print(b_primes)
+	for i in range(2,max_global+1):
+		a_ocu = a_primes.count(i)
+		b_ocu = b_primes.count(i)
+		x = min(a_ocu, b_ocu)
+		#print('x: ' + str(x))
+		#print('i: ' + str(i))
+		factors_p.append(pow(i,x))
+		#print(factors_p)
+
+	print(functools.reduce(mul, factors_p, 1))
+
+
+GCD(1921,9639)
+
 
 def min(x, y): 
 	if x < y: return x
